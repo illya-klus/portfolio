43805da6cfaeef8c0eb885002e48d0c5b34ab34a
illya <illyaklus2@gmail.com>
2026-03-22 00:53:45 +0200

    fix data-json + add info part of profile page

--- src/Profile/Info.tsx
+++ src/Profile/Info.tsx
@@ -1,22 +1,41 @@
+import persomalData from "../data/PersonalData.json";
+import me from "/public/Images/me.jpg";
+
+import { Contacts } from "../widgets/Contacts";
 
 
 const Info = () => {
-    return (
-    <div>
+  return (
+    <div className="flex items-center justify-between">
+      
+      <div className="flex flex-row items-center gap-6"> 
+        <img className="w-32 h-32 bg-gray-300 rounded-full shrink-0" src={me} alt="" />
+
+        <div className="flex flex-col gap-1">
+          <h1 className="text-2xl font-bold text-gray-900">
+            {persomalData.name}
+          </h1>
+  
+          <p className="text-lg font-medium text-gray-800">
+            {persomalData.job}
+          </p>
+  
+          <p className="text-sm text-gray-600 max-w-md">
+            {persomalData.description}
+          </p>
+        </div>
+      </div>
         
+    
+      <div className="flex flex-col gap-2">
+          <h3 className="text-2xl font-bold text-gray-800">Contacts</h3>
+          <Contacts />
+      </div>
+      
     </div>
-    )
-}
+  );
+};
+
+
 
 export default Info;
-
-
-
-
-
-
-
-
-
-
-
---
+++ src/widgets/Contacts.tsx
@@ -0,0 +1,53 @@
+import contactsData from "../data/ContactLinks.json";
+import { Github, Linkedin, Send, Mail } from "lucide-react";
+
+const iconMap: Record<string, any> = {
+  github: Github,
+  linkedin: Linkedin,
+  telegram: Send,
+  mail: Mail,
+};
+
+const colorMap: Record<string, string> = {
+  github: "group-hover:text-white group-hover:drop-shadow-[0_0_10px_white]",
+  linkedin:
+    "group-hover:text-blue-500 group-hover:drop-shadow-[0_0_10px_#3b82f6]",
+  telegram:
+    "group-hover:text-sky-400 group-hover:drop-shadow-[0_0_10px_#38bdf8]",
+  mail: "group-hover:text-green-400 group-hover:drop-shadow-[0_0_10px_#4ade80]",
+};
+
+
+
+type ContactItemProps = {
+  link: string;
+  icon: string;
+};
+
+const ContactItem = ({ link, icon }: ContactItemProps) => {
+  const Icon = iconMap[icon];
+
+  return (
+    <a href={link} target="_blank" rel="noreferrer" className="group relative p-3 rounded-xl bg-neutral-900 hover:bg-neutral-800 hover:scale-102 transition duration-300 ">
+        <Icon className={`w-6 h-6 text-gray-400 transition duration-300 ${colorMap[icon]} `} />
+        <span className=" absolute -bottom-8 left-1/2 -translate-x-1/2 text-xs text-white bg-black px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-200 whitespace-nowrap ">
+            {icon}
+        </span>
+    </a>
+  );
+};
+
+export const Contacts = () => {
+  return (
+      <div className="flex gap-3">
+        {contactsData.map((item) => (
+          <ContactItem
+            key={item.link}
+            icon={item.icon}
+            link={item.link}
+          />
+        ))}
+      </div>
+
+  );
+};
